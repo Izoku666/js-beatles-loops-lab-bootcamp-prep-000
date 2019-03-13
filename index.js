4bf4c8211010b1b4a6bf1001805295e9dd1d1241
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-13 22:26:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,9 @@ function theBeatlesPlay(musicians , instruments)
 function johnLennonFacts(array)
 {
   
+  for(let i = 0; i < array.length; i++)
+  {
+    array[i] = array[i] + `/!/!/!`;
+  }
+  return array;
 }
